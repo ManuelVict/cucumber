7d167850348c37e6e5a109712d0570b19be8d5bb
Manuel <manuelvictmont@gmail.com>
2023-11-21 10:58:00 -0500

    my first test with cucumber

---
+++ src/person.js
@@ -0,0 +1,17 @@
+class Person{
+    
+     moveTo(distance){
+
+    }
+
+    shout(message){
+
+    }
+
+    messageHeard(){
+        return ["free beagle at seam's"]
+
+    }
+
+}
+module.exports = Person
